09c6166248fb073b1490c81ba0105e45bac1eaaf
Oleg Novikov <onovikvo@uncc.edu>
2016-02-29 20:39:25 +0000

    Done with section 4.7.

--- myrottenpotatoes/app/controllers/movies_controller.rb
+++ myrottenpotatoes/app/controllers/movies_controller.rb
@@ -16,6 +16,12 @@ def new
   #default: render 'new' template
 end
 
+def create
+  @movie = Movie.create!(movie_params)
+  flash[:notice] = "#{@movie.title} was successfully created."
+  redirect_to movies_path
+end
+
 private 
   
   def movie_params
